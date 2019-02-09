53d6a94d250957334d2a44f053871c0c61f4e5f0
thatboramy <mcboramy@gmail.com>
2019-02-08 19:23:08 -0700

    Starting

---
+++ DialogTutorial/app/src/main/java/com/example/dialogtutorial/DFragment.java
@@ -0,0 +1,19 @@
+package com.example.dialogtutorial;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DFragment extends DialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.dialogfragment, container,
+                false);
+        getDialog().setTitle("DialogFragment Tutorial");
+        // Do something else
+        return rootView;
+    }
+}
---
+++ DialogTutorial/app/src/main/java/com/example/dialogtutorial/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.dialogtutorial;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends FragmentActivity {
+
+    Button dialogButton;
+    FragmentManager fm = getSupportFragmentManager();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dialogButton = (Button)findViewById(R.id.button);
+
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DFragment dFragment = new DFragment();
+                dFragment.show(fm,"Dialog Fragment");
+            }
+        });
+    }
+
+
+}
