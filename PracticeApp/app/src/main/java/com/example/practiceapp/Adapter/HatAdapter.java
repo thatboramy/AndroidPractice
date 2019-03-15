cc2eda86d370bc57bdd57733ba32546af934f946
thatboramy <mcboramy@gmail.com>
2019-03-15 00:01:43 -0700

    Displays the text

--- PracticeApp/app/src/main/java/com/example/practiceapp/Adapter/HatAdapter.java
+++ PracticeApp/app/src/main/java/com/example/practiceapp/Adapter/HatAdapter.java
@@ -1,4 +1,58 @@
 package com.example.practiceapp.Adapter;
 
-public class HatAdapter {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.practiceapp.R;
+
+public class HatAdapter extends RecyclerView.Adapter<HatAdapter.ViewHolder> {
+
+    Context mcontext;
+    Cursor mcursor;
+
+    public HatAdapter(Context context, Cursor cursor) {
+        mcontext = context;
+        mcursor = cursor;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listitem_hat, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mcursor.getCount();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView hatName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            hatName = (TextView) itemView.findViewById(R.id.hat_textview);
+        }
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int i) {
+        mcursor.moveToPosition(i);
+        //myViewHolder.shirttextView.setText(Shirt.shirts[i].name);
+        //myViewHolder.colortextView.setText(Shirt.shirts[i].color);
+        //holder.bindCursor(mcursor);
+        String currHatName = mcursor.getString(1);
+        holder.hatName.setText(currHatName);
+    }
 }
--- PracticeApp/app/src/main/java/com/example/practiceapp/HatActivity.java
+++ PracticeApp/app/src/main/java/com/example/practiceapp/HatActivity.java
@@ -5,13 +5,21 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.example.practiceapp.Adapter.HatAdapter;
 import com.example.practiceapp.Database.HatDBHelper;
 
 import java.util.ArrayList;
 
 public class HatActivity extends AppCompatActivity {
     public HatDBHelper hatDB;
+
+    public RecyclerView recyclerView;
+    public RecyclerView.Adapter mAdapter;
+    public RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +27,13 @@ public class HatActivity extends AppCompatActivity {
 
         hatDB = new HatDBHelper(this);
         ArrayList<String> name = hatDB.getALLNames();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview_hat);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new HatAdapter(this, hatDB.getallData());
+        recyclerView.setAdapter(mAdapter);
     }
 }
