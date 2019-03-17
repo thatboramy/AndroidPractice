8c2420f78c2c553fb7d9514e0d0be322a176f109
thatboramy <mcboramy@gmail.com>
2019-03-16 23:20:53 -0700

    Attempting to include 2 fragments

--- PracticeApp/app/src/main/java/com/example/practiceapp/BlankFragment.java
+++ PracticeApp/app/src/main/java/com/example/practiceapp/BlankFragment.java
@@ -1,4 +1,4 @@
-package com.example.practiceapp.Fragment;
+package com.example.practiceapp;
 
 import android.content.Context;
 import android.net.Uri;
@@ -8,64 +8,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.practiceapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link HatInputFragment.OnFragmentInteractionListener} interface
+ * {@link BlankFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link HatInputFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
-public class HatInputFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class BlankFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public HatInputFragment() {
+    public BlankFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HatInputFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HatInputFragment newInstance(String param1, String param2) {
-        HatInputFragment fragment = new HatInputFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static Fragment newInstance() {
+        return new BlankFragment();
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hat_input, container);
+        return inflater.inflate(R.layout.fragment_blank, container);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- PracticeApp/app/src/main/java/com/example/practiceapp/MainActivity2.java
+++ PracticeApp/app/src/main/java/com/example/practiceapp/MainActivity2.java
@@ -1,21 +1,20 @@
 package com.example.practiceapp;
 
-import android.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.practiceapp.Fragment.HatInputFragment;
+import com.example.practiceapp.ui.mainactivity2.MainActivity2Fragment;
 
-public class HatInputActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hat_input);
+        setContentView(R.layout.main_activity2_activity);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, BlankFragment.newInstance());
+                    .replace(R.id.container, MainActivity2Fragment.newInstance())
+                    .commitNow();
         }
     }
 }
